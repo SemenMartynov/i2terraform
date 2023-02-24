454123cce42074fb75b6f758636491fc185677e5
Semen Martynov <semen.martynov@gmail.com>
2023-02-24 23:00:55 +0300

    The first web server + security group

---
+++ simpleservice.tf
@@ -0,0 +1,54 @@
+# ------------------------------
+# Simple web-service
+# ------------------------------
+
+provider "aws" {
+#  access_key = "AKIA3BI6WTWR7ZDWH64O"
+#  secret_key = "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"
+  region     = "eu-north-1" # Stockholm
+}
+
+resource "aws_instance" "my_webserver" {
+  ami                    = "ami-0bb935e4614c12d86" # Amazon Linux 2 Kernel 5.10
+  instance_type          = "t3.micro"              # 2vCPU, 1G
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform!" > /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+
+  tags = {
+    Name  = "Web Server build by Terraform"
+    Owner = "Semen Martynov"
+  }
+}
+
+resource "aws_security_group" "my_webserver" {
+  name        = "WebServer Security Group"
+  description = "My First SecurityGroup"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Recieve http request"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "Web Server SecurityGroup"
+    Owner = "Semen Martynov"
+  }
+}
