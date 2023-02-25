393f6507eaf20dbfb78b22b9ffb5a276be0d16d7
Semen Martynov <semen.martynov@gmail.com>
2023-02-25 12:09:40 +0300

    Template files

--- simpleservice.tf
+++ simpleservice.tf
@@ -12,7 +12,11 @@ resource "aws_instance" "my_webserver" {
   ami                    = "ami-0bb935e4614c12d86" # Amazon Linux 2 Kernel 5.10
   instance_type          = "t3.micro"              # 2vCPU, 1G
   vpc_security_group_ids = [aws_security_group.my_webserver.id]
-  user_data              = file("userdata.sh")
+  user_data              = templatefile("userdata.sh.tpl", {
+    this_month = "February",
+    next_month = "March",
+    week_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+  })
 
   tags = {
     Name  = "Web Server build by Terraform"
--- userdata.sh.tpl
+++ userdata.sh.tpl
@@ -1,7 +1,22 @@
 #!/bin/bash
 yum -y update
 yum -y install httpd
+
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform!" > /var/www/html/index.html
+
+cat <<EOF > /var/www/html/index.html
+<html>
+<h2>WebServer with IP: $myip</h2><br>Build by Terraform!<br>
+This month is ${this_month}, next month is ${next_month}<br>
+
+<br>
+
+%{ for val in week_days ~}
+I like ${val}<br>
+%{ endfor ~}
+
+</html>
+EOF
+
 sudo service httpd start
 chkconfig httpd on
