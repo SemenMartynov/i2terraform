db30519481b7b8665b92f70c2afbf7e848c9310c
Semen Martynov <semen.martynov@gmail.com>
2023-02-26 11:19:29 +0300

    Highly Available Web Cluster

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,13 @@
-output "webserver_instance_id" {
-  value = aws_instance.my_webserver.id
-  description = "WebServer instance ID"
-}
-
-output "webserver_public_ip_address" {
-  value = aws_eip.webserver_static_ip.public_ip
-  description = "WebServer public IP"
+output "web_load_balancer_url" {
+  value = aws_elb.web.dns_name
 }
 
 output "webserver_sg_id" {
-  value = aws_security_group.my_webserver.id
+  value = aws_security_group.webserver.id
   description = "WebServer Security Group ID"
 }
 
 output "webserver_sg_arn" {
-  value = aws_security_group.my_webserver.arn
+  value = aws_security_group.webserver.arn
   description = "WebServer Security Group ARN"
 }
